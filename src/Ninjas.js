e10314f87dcdf3b76ca8d11e362dc7fb9e2eb9ee
Thenuka Dinu <thenukadinu@gmail.com>
2020-01-02 00:32:01 +0530

    conditions

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,23 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-    render() {
-        const { ninjas } = this.props; // Destructuring array return from App.js(root) Component
-        const ninjaList = ninjas.map(ninja => { /*Map all ninjas using unique key or id*/
-            return (
-                <div className="ninja" key={ ninja.id }>
-                    <div>Name: { ninja.name }</div>
-                    <div>Age: { ninja.age }</div>
-                    <div>Belt: { ninja.belt }</div>
-                </div>
-            )
-        })
-        return (
-            <div className="ninja-list">
-            { ninjaList /*Show all ninjas data return from root */ }
-            </div>
-        )
-    }
+const Ninjas = ({ninjas}) => {  /* Destructuring directly array return from App.js (root) Component*/  
+
+    /*const { ninjas } = props; // Destructuring array return from App.js (root) Component */
+
+    // const ninjaList = ninjas.map(ninja => { /*Map all ninjas using unique key or id*/
+    //     if (ninja.age > 20) {
+    //         return (
+    //             <div className="ninja" key={ ninja.id }>
+    //                 <div>Name: { ninja.name }</div>
+    //                 <div>Age: { ninja.age }</div>
+    //                 <div>Belt: { ninja.belt }</div>
+    //             </div>
+    //         )
+    //     } else {
+    //         return null
+    //     }
+    // })
+
+    const ninjaList = ninjas.map(ninja => {
+       return ninja.age > 20 ? (
+        <div className="ninja" key={ ninja.id }>
+            <div>Name: { ninja.name }</div>
+            <div>Age: { ninja.age }</div>
+            <div>Belt: { ninja.belt }</div>
+        </div>
+        ) : null
+    })
+    return (
+        <div className="ninja-list">
+        { ninjaList /*Show all ninjas data return from root */ }
+        </div>
+    )
 }
 
 export default Ninjas
