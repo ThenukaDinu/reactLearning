6bd997e7aa5d8fad4bed21fdccda06cac75527b8
Thenuka Dinu <thenukadinu@gmail.com>
2020-01-01 23:17:42 +0530

    Outputting a list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My First React App</h1>
-      <p>Welcome</p>
-    </div>
-  );
+class App extends Component {
+
+  //Array of objects 
+  state = {
+    ninjas : [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My First React App</h1>
+        <p>Welcome </p>
+        <Ninjas ninjas={ this.state.ninjas  /* Pass data to child component*/ } /> 
+      </div>
+    );  
+  }
 }
 
 export default App;
