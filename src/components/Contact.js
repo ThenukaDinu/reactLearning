66ed72b868f58c8e9e3b7bc7ad9d37c972f9d3f3
Thenuka Dinu <thenukadinu@gmail.com>
2020-01-14 22:16:02 +0530

    Ridirecting

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Contact = () => {
-    return(
+const Contact = (props) => {
+    //Redirect using history object push method in props
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000)
+
+    return (
         <div className="container">
             <h4 className="center">Contact Page</h4>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure amet, ut fuga architecto quae harum quasi veniam. Officiis distinctio facere sint sequi sunt laudantium, minus dolore ratione quia porro culpa.</p>
